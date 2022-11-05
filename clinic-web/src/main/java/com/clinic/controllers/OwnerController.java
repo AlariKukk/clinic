0c6adba97990d740cb5342b82e85c45dd0cd8000
Alari Kukk <alari.kukk@industry62.com>
2022-11-05 10:48:36 +0200

    Fixed broken Links. closes #46

--- clinic-web/src/main/java/com/clinic/controllers/OwnerController.java
+++ clinic-web/src/main/java/com/clinic/controllers/OwnerController.java
@@ -4,6 +4,7 @@ import com.clinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
@@ -14,9 +15,15 @@ public class OwnerController {
         this.ownerService = ownerService;
     }
 
-    @RequestMapping({"","/", "/index", "/index.html"})
+    @RequestMapping({"", "/", "/index", "/index.html"})
     public String listOwners(Model model) {
         model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
+    }
+
+
+    @RequestMapping("/find")
+    public String findOwners() {
+        return "notimplemented";
     }
 }
