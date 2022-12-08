384ea61b29938da280564fed9ecb7fd40ac880b6
Alari Kukk <alari.kukk@industry62.com>
2022-12-08 18:55:16 +0200

    Updated owner with JPA entity mappings. closes #36

--- clinic-data/src/main/java/com/clinic/model/Owner.java
+++ clinic-data/src/main/java/com/clinic/model/Owner.java
@@ -1,13 +1,22 @@
 package com.clinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "owners")
 public class Owner extends Person {
 
+    @Column(name = "address")
     private String address;
+    @Column(name = "city")
     private String city;
+    @Column(name = "telephone")
     private String telephone;
+
+    //for pets we have to set up a relationship mapping. @OneToMany so that we are going to have many pets here.
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner") //cascade type means that if i delete an owner then that is going to cascade down, if i have pets and i delete the owner of the pet then the pets will also get deleted.
     private Set<Pet> pets = new HashSet<>();
 
     public String getAddress() {
--- clinic-data/src/main/java/com/clinic/model/Person.java
+++ clinic-data/src/main/java/com/clinic/model/Person.java
@@ -1,8 +1,14 @@
 package com.clinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass //the way we are setting up the mapping, we will never work with a Person POJO per se. We are going to be extending out from that.
 public class Person extends BaseEntity {
 
+    @Column(name = "first_name") //it's not actually needed to do that because hibernate would do that for us by default.
     private String firstName;
+    @Column(name = "last_name") //it's not actually needed to do that because hibernate would do that for us by default.
     private String lastName;
 
     public String getFirstName() {
